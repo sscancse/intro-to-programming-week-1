8739b79aa34196fe1773bef8a8c099edacd3aa83
Seamus_Scanlon <Seamus_C_Scanlon@Progressive.com>
2022-12-07 10:01:10 -0500

    GoldAccount Initial Commit

---
+++ BankingSolution/BankingUnitTests/GoldAccountBenefits.cs
@@ -0,0 +1,20 @@
+﻿using Banking.Domain;
+
+namespace Banking.UnitTests
+{
+    public class GoldAccountBenefits
+    {
+        [Fact]
+        public void GetBonusOnDeposit()
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            var deposit = 100M;
+            var expectedBonus = 10M;
+
+            account.Deposit(deposit);
+
+            Assert.Equal(openingBalance + deposit + expectedBonus, account.GetBalance());
+        }
+    }
+}
